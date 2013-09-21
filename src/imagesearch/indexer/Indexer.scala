3405716050a9da9874a1593933f8bd24cfb9417f
Ubuntu <ubuntu@ip-10-117-35-208.ec2.internal>
2013-09-21 07:21:03 +0000

    Added indexer and wordCount functions.  Simpler tokenization.

---
+++ src/imagesearch/indexer/Indexer.scala
@@ -0,0 +1,19 @@
+package imagesearch.Indexer.indexer.scala
+
+/**
+ * John Dulin
+ * September 20, 2013
+ */
+object Indexer {
+
+	def index(page: String) {
+		var tokens = page.split("//s+")		
+		
+	}
+
+	def wordCount(words: Array[String]) {
+
+	}
+
+}
+
