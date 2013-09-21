d37703f9d7f59f91fb154f53657549ea1b220f67
Brendan Higgins <bjh83@case.edu>
2013-09-21 06:01:27 -0400

    I added some database stuff

--- src/imagesearch/textingestor/HeadIngestor.scala
+++ src/imagesearch/textingestor/HeadIngestor.scala
@@ -2,6 +2,8 @@ package imagesearch.textingestor
 
 import org.jsoup.nodes.Document
 import imagesearch.utilities.Ingestor
+import com.mongodb.DBCollection
+import com.mongodb.casbah.commons.MongoDBObject
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,8 +12,12 @@ import imagesearch.utilities.Ingestor
  * Time: 2:25 AM
  * To change this template use File | Settings | File Templates.
  */
-class HeadIngestor extends Ingestor[Document, DocumentBundle] {
+class HeadIngestor(private val unvisitedCol: DBCollection) extends Ingestor[Document, DocumentBundle] {
   override def ingest(doc: Document): DocumentBundle = {
 
+  }
+
+  def addUnvisitedURLs(urls: List[String]) {
+    urls.map(url => MongoDBObject("url" -> url, "visited" -> false)).foldLeft(unvisitedCol) { case(col, obj) => col.insert(obj) }
   }
 }
