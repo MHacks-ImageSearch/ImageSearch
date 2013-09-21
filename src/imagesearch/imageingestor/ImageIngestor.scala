a0019cdec55f17a13df969acca7b1eff3317a939
Brendan Higgins <bjh83@case.edu>
2013-09-21 01:03:43 -0400

    I created the package hierarchy

---
+++ src/imagesearch/imageingestor/ImageIngestor.scala
@@ -0,0 +1,12 @@
+package imagesearch.imageingestor.index
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brendan
+ * Date: 9/21/13
+ * Time: 12:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class ImageIngestor {
+
+}
