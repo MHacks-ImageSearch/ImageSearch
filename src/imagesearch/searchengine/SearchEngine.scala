0be2d2e7a5a43503189b587b5212da44d65acc66
Brendan Higgins <bjh83@case.edu>
2013-09-22 05:05:25 -0400

    I wrote some utilities for the search engine.

--- src/imagesearch/searchengine/SearchEngine.scala
+++ src/imagesearch/searchengine/SearchEngine.scala
@@ -1,5 +1,9 @@
 package imagesearch.searchengine
 
+import imagesearch.imageingestor.NormalizedRGBHistogram
+import imagesearch.imageingestor.imageutils.NormalizedHistogram
+import Math.abs
+
 /**
  * Created with IntelliJ IDEA.
  * User: brendan
@@ -9,4 +13,26 @@ package imagesearch.searchengine
  */
 class SearchEngine {
 
+}
+
+object SearchEngine {
+  def compareHistograms(leftHist: NormalizedHistogram, rightHist: NormalizedHistogram): Float = {
+    leftHist.zip(rightHist).map{ case(left, right) => abs(left - right) }.reduce{ case(left, right) => left + right }
+  }
+
+  def compareHistograms(leftHist: NormalizedRGBHistogram, rightHist: NormalizedRGBHistogram): Float = {
+    val (leftRed: NormalizedHistogram, leftGreen: NormalizedHistogram, leftBlue: NormalizedHistogram) = leftHist
+    val (rightRed: NormalizedHistogram, rightGreen: NormalizedHistogram, rightBlue: NormalizedHistogram) = rightHist
+    return (compareHistograms(leftRed, rightRed) + compareHistograms(leftGreen, rightGreen) + compareHistograms(leftBlue, rightBlue)) / 3
+  }
+
+  def maxIndex(hist: NormalizedHistogram): Int = {
+    var max = 0
+    for(index <- 0 until hist.length) {
+      if(hist(index) > hist(max)) {
+        max = index
+      }
+    }
+    return max
+  }
 }
